524225ad137f3d6957fd6a6b98597c8d61900da3
Tiago Lelis <130498904+Lelis19@users.noreply.github.com>
2025-03-20 18:42:14 -0300

    Add files via upload

---
+++ App.js
@@ -0,0 +1,14 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+import CartaoPerfil from './src/components/CartaoPerfil'
+
+
+const App = () => {
+  return (
+    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+      <CartaoPerfil/>
+    </View>
+  )
+}
+
+export default App
